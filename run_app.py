cf00baac434dabcde5334dc5b7f9ee64725e26bb
Paulo R S Oliveira <proberto3122@gmail.com>
2025-02-23 10:40:22 +0000

    Atualizacao fhs

--- run_app.py
+++ run_app.py
@@ -1,7 +1,7 @@
 import os
 
 # Caminho relativo para o seu script Streamlit
-script_path = os.path.join(os.path.dirname(__file__), 'streamlit_fhs.py')
+script_path = os.path.join(os.path.dirname(__file__), 'model_fhs.py')
 
 # Comando para executar o script Streamlit
 os.system(f'streamlit run {script_path}')
